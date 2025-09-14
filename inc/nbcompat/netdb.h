177b474ba883ee412de9bf1dcbd9b7717ac4315e
darealshinji <djcj@gmx.de>
2025-09-14 15:15:29 +0200

    guard fake nbcompat headers

--- inc/nbcompat/netdb.h
+++ inc/nbcompat/netdb.h
@@ -1,1 +1,8 @@
+#ifndef _NBCOMPAT_NETDB_H
+#define _NBCOMPAT_NETDB_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netdb.h>
+
+#endif /* !_NBCOMPAT_NETDB_H */
--- inc/nbcompat/stdio.h
+++ inc/nbcompat/stdio.h
@@ -1,1 +1,7 @@
+#ifndef _NBCOMPAT_STDIO_H_
+#define _NBCOMPAT_STDIO_H_
+
+#include <stdarg.h>
 #include <stdio.h>
+
+#endif	/* !_NBCOMPAT_STDIO_H_ */
